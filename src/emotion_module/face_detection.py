45f6cc56c5cc1a5971f2b1745281df922621c26c
amineChadli <amine.chadli.ensias@gmail.com>
2018-01-23 21:05:33 +0100

    Update face_detection.py

--- src/emotion_module/face_detection.py
+++ src/emotion_module/face_detection.py
@@ -9,7 +9,7 @@ Created on Sun Jan 14 13:20:34 2018
 import cv2
 def is_face_detected(image):
     
-    face_cascade = cv2.CascadeClassifier('/home/amine/projets/Emotions_project/lbpcascade_frontalface_improved.xml')
+    face_cascade = cv2.CascadeClassifier('lbpcascade_frontalface_improved.xml')
     faces_result = face_cascade.detectMultiScale(image, 1.3, 5)
     if(len(faces_result)!=0):
         return True,faces_result
